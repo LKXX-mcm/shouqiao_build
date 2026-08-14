cb487192d0c72375af7508c9e4665b6e10dc0d2e
lkx <likaixiang@microbeam.cn>
2026-08-14 17:16:06 +0800

    001

---
+++ web-mobile-001/assets/top-story-scroll-text/index.48d63.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/top-story-scroll-text",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/top-story-scroll-text', 'chunks:///_virtual/top-story-scroll-text'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
