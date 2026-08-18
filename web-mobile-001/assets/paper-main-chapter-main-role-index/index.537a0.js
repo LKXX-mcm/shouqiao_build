b6e5d6d2a59b7dfb6fb801d5b5cae55eb47c0daf
lkx <likaixiang@microbeam.cn>
2026-08-18 17:06:38 +0800

    v0.0.5

---
+++ web-mobile-001/assets/paper-main-chapter-main-role-index/index.537a0.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/paper-main-chapter-main-role-index",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/paper-main-chapter-main-role-index', 'chunks:///_virtual/paper-main-chapter-main-role-index'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
